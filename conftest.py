8166bfd848800781c1b726e9bbd8b15dc6b70e70
Ida Rahnama <ida.rahnama@Idas-MacBook-Pro.local>
2020-06-03 22:43:48 +1000

    Add comments for tests

--- conftest.py
+++ conftest.py
@@ -5,6 +5,7 @@ import unittest
 
 class FlaskTestCase(unittest.TestCase):
     def test_positive_route_company(self):
+        """Verify that the API works for company employees."""
         tester = APP.test_client(self)
         response = tester.get(
             "/paranuara/company/COWTOWN", content_type="application/json"
@@ -12,6 +13,7 @@ class FlaskTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_negative_route_company(self):
+        """Verify that the API returns error on wrong link for company employees."""
         tester = APP.test_client(self)
         response = tester.get(
             "/paranuara/companIES/COWTOWN", content_type="application/json"
@@ -19,13 +21,15 @@ class FlaskTestCase(unittest.TestCase):
         self.assertNotEqual(response.status_code, 200)
 
     def test_positive_route_person(self):
+        """Verify that the API works for person favorite fruits and vegetables."""
         tester = APP.test_client(self)
         response = tester.get(
             "/paranuara/person/Decker%20Mckenzie", content_type="application/json"
         )
         self.assertEqual(response.status_code, 200)
 
-    def test_negative_route_people(self):
+    def test_negative_route_person(self):
+        """Verify that the API returns error on wrong link for person favorite fruits and vegetables."""
         tester = APP.test_client(self)
         response = tester.get(
             "paranuara/persON/Decker%20Mckenzie", content_type="application/json"
@@ -33,6 +37,7 @@ class FlaskTestCase(unittest.TestCase):
         self.assertNotEqual(response.status_code, 200)
 
     def test_positive_route_people(self):
+        """Verify that the API works for the friends names."""
         tester = APP.test_client(self)
         response = tester.get(
             "/paranuara/people/?person1=Decker%20Mckenzie&person2=Rosemary%20Hayes",
@@ -40,7 +45,8 @@ class FlaskTestCase(unittest.TestCase):
         )
         self.assertEqual(response.status_code, 200)
 
-    def test_negative_route_person(self):
+    def test_negative_route_people(self):
+        """Verify that the API returns error on wrong link for friends names."""
         tester = APP.test_client(self)
         response = tester.get(
             "paranuara//peOPle/person1=Decker%20Mckenzie&person2=Rosemary%20Hayes",
