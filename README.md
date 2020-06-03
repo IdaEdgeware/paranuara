1d15b9d7481ed0c8bff1e2112cc1fc89d5533354
Ida Rahnama <ida.rahnama@Idas-MacBook-Pro.local>
2020-06-03 19:08:52 +1000

    modify readme and add more tests

--- README.md
+++ README.md
@@ -5,15 +5,24 @@
 | --- | --- | --- |
 | GET | http://[hostname:port]/paranuara/company/[company_name] | Retrieve list of all employees of the company.e.g http://localhost:8080/paranuara/company/BUGSALL |
 | GET | http://[hostname:port]/paranuara/person/[person_name] | Given 1 people, provide a list of fruits and vegetables they like. e.g. http://localhost:8080/paranuara/person/Decker%20Mckenzie |
-| GET | http://[hostname:port]/paranuara/people/?person1=[person1]&person2=[person2] | Given 2 people, retrieve information of both people and the list of their friends in common which have brown eyes and are still alive. e.g. http://localhost:8080/people/person1=Decker%20Mckenzie&person2=Rosemary%20Hayes|
+| GET | http://[hostname:port]/paranuara/people/?person1=[person1]&person2=[person2] | Given 2 people, retrieve information of both people and the list of their friends in common which have brown eyes and are still alive. e.g. http://localhost:8080/paranuara/people/?person1=Decker%20Mckenzie&person2=Rosemary%20Hayes|
 
 # How to Install
 1. Pull Docker image 
     - `docker pull idarahnama/paranuara`
 2. Run docker container 
-    - `docker run [--name <any_name>] -p <local_port>:5000 idarahnama/paranuara`
+    - `docker run [--name <any_name>] -p <local_port>:5000 -t idarahnama/paranuara`
 3. API would now be accessible on local port specified in previous command
 
+# How to Run Test
+1. Install requirements
+    - `pip3 install -r req.txt`
+2. Run the test files 
+    - `python3 conftest.py`
+3. Returns the result of tests
+    - `Ran 6 tests in 90.564s`
+    - `OK`
+
 # To-Do
-1. Add test cases for all end points
+1. More tests to check the functionalty
 2. Improve excpetion scenarios for different endpoints 
--- conftest.py
+++ conftest.py
@@ -2,16 +2,52 @@ from app import APP
 import pytest
 import unittest
 
+
 class FlaskTestCase(unittest.TestCase):
     def test_positive_route_company(self):
         tester = APP.test_client(self)
-        response = tester.get('/paranuara/company/COWTOWN', content_type='application/json')
+        response = tester.get(
+            "/paranuara/company/COWTOWN", content_type="application/json"
+        )
         self.assertEqual(response.status_code, 200)
-    
+
     def test_negative_route_company(self):
         tester = APP.test_client(self)
-        response = tester.get('/paranuara/companIES/COWTOWN', content_type='application/json')
+        response = tester.get(
+            "/paranuara/companIES/COWTOWN", content_type="application/json"
+        )
         self.assertNotEqual(response.status_code, 200)
 
-if __name__ == '__main__':
+    def test_positive_route_person(self):
+        tester = APP.test_client(self)
+        response = tester.get(
+            "/paranuara/person/Decker%20Mckenzie", content_type="application/json"
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_negative_route_people(self):
+        tester = APP.test_client(self)
+        response = tester.get(
+            "paranuara/persON/Decker%20Mckenzie", content_type="application/json"
+        )
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_positive_route_people(self):
+        tester = APP.test_client(self)
+        response = tester.get(
+            "/paranuara/people/?person1=Decker%20Mckenzie&person2=Rosemary%20Hayes",
+            content_type="application/json",
+        )
+        self.assertEqual(response.status_code, 200)
+
+    def test_negative_route_person(self):
+        tester = APP.test_client(self)
+        response = tester.get(
+            "paranuara//peOPle/person1=Decker%20Mckenzie&person2=Rosemary%20Hayes",
+            content_type="application/json",
+        )
+        self.assertNotEqual(response.status_code, 200)
+
+
+if __name__ == "__main__":
     unittest.main()
